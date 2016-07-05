fd5b8c312785b12fa88722bb6af097a07d6c8e4e
SabrinaMH <sabrina_privat@hotmail.com>
2016-07-05 19:45:56 +0200

    Had moved events around such that they obtained new namespaces. Of course it causes problems with GES.

---
+++ TaskManager.Test.FSharp/TaskManager.Test.FSharp.fs
@@ -0,0 +1,20 @@
+﻿module TaskManager.Test.FSharp
+
+open FsUnit
+open FsCheck
+open NUnit.Framework
+open NUnitRunner
+open TaskManager.Domain.Models.Task
+open TaskManager.Domain.Models.Project
+open TaskManager.Domain.Models.Common
+
+[<Test>]
+let shouldSayHello2
+    = fun () -> Assert.AreEqual(2, 2)
+
+[<Test>]
+let ``Erase note on task raises event`` () =
+    let task = new Task(new ProjectId("projectId"), new Title("task title"), TaskPriority.Low)
+    let uncommittedEvents = task.GetUncommittedEvents()
+    let containsEvent event = (event = new NoteErased(task.Id.ToString()))
+    Assert.True(Seq.exists containsEvent uncommittedEvents)
